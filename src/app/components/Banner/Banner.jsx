d896331489769ecd62c1c4784ed744a68015701b
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-12-04 02:12:51 -0800

    added

--- src/app/components/Banner/Banner.jsx
+++ src/app/components/Banner/Banner.jsx
@@ -4,10 +4,10 @@ return(
    <div>
     <figure className="relative">
         <img src="https://i.ibb.co.com/ZptnqZR1/Group-3473642.png" alt="product image" />
-        
+
     </figure>
    </div>
 )
 }
 
-export default Banner
+export default Banner 
